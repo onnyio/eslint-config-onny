ad77fc265ea311e82b7b1e7ef7531b65d5b53130
Isaac Marotte <imarotte@gmail.com>
2019-07-21 23:17:59 -0700

    some rule clean up
    * switch to ecmaVersion: 2018
    * turn off react/no-did-update-set-state
    * turn on import/first
    * no-unused-vars no longer ignores args

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-/**
- * Copyright (C) 2015-2016 Onny LLC - All Rights Reserved
+/*
+ * @Copyright (C) 2015-2019 Onny LLC - All Rights Reserved
+ *
+ * This file is part of eslint-config-onny and is the sole property of its owner.
+ * Unauthorized use of this file, via any medium or form, whole or in part,
+ * is strictly prohibited without the expressed written permission of Onny LLC and/or Isaac Marotte
+ *
+ * This file is proprietary and confidential
+ *
+ * Last Modified: 2019.7.21
  */
 
 
@@ -16,8 +24,8 @@ module.exports = {
   },
   parser: 'babel-eslint',
   parserOptions: {
+    ecmaVersion: 2018,
     ecmaFeatures: {
-      experimentalObjectRestSpread: true,
       jsx: true,
       modules: true,
       decorators: true
@@ -56,34 +64,62 @@ module.exports = {
     // Turn Off
     /////////////////////
     radix: 0,
-    "function-paren-newline": 0,
-    "prefer-destructuring": 0,
-    // not needed in ES5 or later
+    'function-paren-newline': 0,
+    'prefer-destructuring': 0,
+
+    /**
+     * not needed in ES5 or later
+     */
     'no-else-return': 0,
-    // makes the code more readable
-    'import/first': [0, 'DISABLE-absolute-first'],
+
+    /**
+     * This rule reports any imports that come after non-import statements.
+     *
+     * @see [https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/first.md#importfirst]{@link https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/first.md#importfirst}
+     */
+    'import/first': [2, 'absolute-first'],
+
+    /**
+     * @see [https://reactjs.org/docs/react-component.html#componentdidupdate]{@link https://reactjs.org/docs/react-component.html#componentdidupdate}
+     * @see [https://github.com/airbnb/javascript/issues/1875]{@link https://github.com/airbnb/javascript/issues/1875}
+     *
+     * The React docs say calling setState() in componentDidUpdate() is permitted, albeit with caveats.
+     *
+     * You may call setState() immediately in componentDidUpdate() but note that it must be
+     * wrapped in a condition like in the example above, or you’ll cause an infinite loop.
+     * It would also cause an extra re-rendering which, while not visible to the user,
+     * can affect the component performance.
+     */
+    'react/no-did-update-set-state': 0,
 
 
     /////////////////////
     // Warnings
     /////////////////////
     semi: [1, 'always'],
-    'no-unused-vars': [1, { args: 'none' }],
-    // allows us to retain args for API calls
+
+    /**
+     * Variables that are declared and not used anywhere in the code are most likely an error
+     * due to incomplete refactoring. Such variables take up space in the code
+     * and can lead to confusion by readers
+     * @see [https://eslint.org/docs/rules/no-unused-vars]{@link https://eslint.org/docs/rules/no-unused-vars}
+     */
+    'no-unused-vars': ['error', { vars: 'all', args: 'after-used', ignoreRestSiblings: false }],
+
     'linebreak-style': [1, 'unix'],
 
 
     /////////////////////
     // Errors
     /////////////////////
-    "object-curly-newline": ["error", {
-      "multiline": true,
-      "consistent": true
+    'object-curly-newline': ['error', {
+      multiline: true,
+      consistent: true
     }],
-    "jsx-a11y/anchor-is-valid": [ "error", {
-      "components": [ "Link" ],
-      "specialLink": [ "to"],
-      "aspects": [ "noHref", "preferButton" ]
+    'jsx-a11y/anchor-is-valid': ['error', {
+      components: ['Link'],
+      specialLink: ['to'],
+      aspects: ['noHref', 'preferButton']
     }],
     indent: [2, 2, { SwitchCase: 1, VariableDeclarator: 1, outerIIFEBody: 1 }],
     'max-len': [2, { code: 100, tabWidth: 4, ignoreComments: true }],
@@ -102,8 +138,8 @@ module.exports = {
           '__INITIAL_STATE__',
           '__DEVELOPMENT__',
           '__REDUX_DEVTOOLS_EXTENSION_COMPOSE__',
-          "_reCAPTCHA_SECRET_KEY_",
-          "__test__"
+          '_reCAPTCHA_SECRET_KEY_',
+          '__test__'
         ]
       }
     ],
